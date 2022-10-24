c0781c85144fcd1d7c8a706c8b075ddc67e105d7
mariuf17 <mariuf17@uia.no>
2022-10-24 13:40:12 +0200

    Bruker-modell
    Lagde en klasse for bruker
    Lagde get og set metoder for alle attributtene
    La inn Bruker_ID som PK
    La inn Bruker_ID som Required

---
+++ NordicDoor/NordicDoor/Models/Bruker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace NordicDoor.Models
+{
+    public class Bruker
+    {
+        [Key]
+        public int Bruker_Id { get; set; }
+        [Required]
+
+        public string Postnummer { get; set; }
+
+        public string Navn { get; set; }
+
+        public string Epost { get; set; }
+
+        public int Telefon { get; set; }
+
+    }
+}
+
