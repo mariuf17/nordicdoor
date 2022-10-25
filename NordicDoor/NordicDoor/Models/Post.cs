b64714424c1a45d1742eed4937ee53c477cf0d78
mariuf17 <mariuf17@uia.no>
2022-10-25 10:52:54 +0200

    Post-modell

---
+++ NordicDoor/NordicDoor/Models/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace NordicDoor.Models
+{
+    public class Post
+    {
+        [Key]
+        public string Postnummer { get; set; }
+        [Required]
+
+        public string Adresse { get; set; }
+
+    }
+}
+
