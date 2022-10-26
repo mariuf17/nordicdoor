e9bec1541d63649584c46fd6654b97e23285e51f
mariuf17 <mariuf17@uia.no>
2022-10-26 13:14:20 +0200

    Fikset connection string

--- NordicDoor/NordicDoor/Program.cs
+++ NordicDoor/NordicDoor/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
-    builder.Configuration.GetConnectionString("MariaDb")
+    builder.Configuration.GetConnectionString("DefaultConnection")
     ));
 
 var app = builder.Build();
