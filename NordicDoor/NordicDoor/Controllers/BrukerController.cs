0288f4995803190740dc59a834feb334b062a508
mariuf17 <mariuf17@uia.no>
2022-11-03 12:49:23 +0100

    Funksjon som lar deg redigere brukere i listen
    La inn en funksjon som gjør det mulig å redigere brukere som ligger i listen. Den lagrer ikke endringene enda, så gir Exception etter lagring.

--- NordicDoor/NordicDoor/Controllers/BrukerController.cs
+++ NordicDoor/NordicDoor/Controllers/BrukerController.cs
@@ -30,13 +30,53 @@ public class BrukerController : Controller
     //POST
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public IActionResult Opprett(Bruker obj )
+    public IActionResult Opprett(Bruker obj)
     {
-        if(obj.Navn == obj.Ansatt_ID.ToString())
+        if (obj.Navn == obj.Ansatt_ID.ToString())
         {
             ModelState.AddModelError("CustomError", "Navn og Ansatt_ID kan ikke inneholde like verdier");
         }
-     
+
+        if (ModelState.IsValid)
+        {
+            _first.Bruker.Add(obj);
+            _first.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        return View(obj);
+    }
+
+
+    //GET
+    public IActionResult Rediger(int? Ansatt_ID)
+    {
+        {
+            if (Ansatt_ID==null || Ansatt_ID==0)
+                return NotFound();
+        }
+        var brukerFromFirst = _first.Bruker.Find(Ansatt_ID);
+        //var brukerFromFirstFirst = _first.Bruker.FirstOrDefault(u => u.id == id);
+        //var brukerFromFirstSingle = _first.Bruker.SingleOrDefault(u => u.id == id);
+
+        if(brukerFromFirst == null)
+        {
+            return NotFound();
+        }
+
+
+        return View(brukerFromFirst);
+    }
+
+    //POST
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Rediger(Bruker obj)
+    {
+        if (obj.Navn == obj.Ansatt_ID.ToString())
+        {
+            ModelState.AddModelError("CustomError", "Navn og Ansatt_ID kan ikke inneholde like verdier");
+        }
+
         if (ModelState.IsValid)
         {
             _first.Bruker.Add(obj);
