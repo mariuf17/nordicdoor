3c3958ef016f90d1394c6e40cc58dbf6b4da4867
ThomasTvedten <thomastv@uia.no>
2022-11-10 10:21:10 +0100

    Team_medlemmer_controller

--- NordicDoor/NordicDoor/Models/TeamMedlemmer.cs
+++ NordicDoor/NordicDoor/Models/TeamMedlemmer.cs
@@ -4,13 +4,13 @@ using System.ComponentModel.DataAnnotations;
 namespace NordicDoor.Models
 {
 
-    public class TeamMedlemmer
+    public class Team_Medlemmer
     {
         [Key]
-        public int Bruker_ID { get; set; }
+        public int Team_ID { get; set; }
         [Required]
 
-        public int Team_ID { get; set; }
+        public int Ansatt_ID { get; set; }
 
     }
 }
