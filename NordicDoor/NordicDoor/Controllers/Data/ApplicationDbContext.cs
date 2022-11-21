52682d6b8f49ea7285af3c488b56e8f1c8869b1e
ThomasTvedten <thomastv@uia.no>
2022-11-21 09:32:49 +0100

    fiksa_side

--- NordicDoor/NordicDoor/Controllers/Data/ApplicationDbContext.cs
+++ NordicDoor/NordicDoor/Controllers/Data/ApplicationDbContext.cs
@@ -8,29 +8,31 @@ using NordicDoor.Entities;
 namespace NordicDoor.Controllers.Data;
 
 
-    public class ApplicationDbContext : DbContext
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-        {
-        }
+    }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<UserEntity>().HasKey(x => x.Id);
-            base.OnModelCreating(modelBuilder);
-        }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<UserEntity>().HasKey(x => x.Id);
+        base.OnModelCreating(modelBuilder);
+    }
 
-        public DbSet<UserEntity> Users { get; set; }
+    public DbSet<UserEntity> Users { get; set; }
 
-        public DbSet<Bruker> Bruker { get; set; }
+    public DbSet<Bruker> Bruker { get; set; }
 
-        public DbSet<Roller> Roller { get; set; }
+    public DbSet<Roller> Roller { get; set; }
 
-        public DbSet<Forslag> Forslag { get; set; }
+    public DbSet<Forslag> Forslag { get; set; }
 
-        public DbSet<Team_Medlemmer> Team_Medlemmer { get; set; }
+    public DbSet<Team_Medlemmer> Team_Medlemmer { get; set; }
 
-        public DbSet<Team> Team { get; set; }
+    public DbSet<Team> Team { get; set; }
+
+    public DbSet<Forslag_Status> Forslag_Status { get; set; }
 
     }
 
---
+++ NordicDoor/NordicDoor/Controllers/Data/Forslag_StatusController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NordicDoor.Controllers.Data;
+using NordicDoor.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NordicDoor.Controllers;
+
+public class Forslag_StatusController : Controller
+{
+    private readonly ApplicationDbContext _first;
+
+    public Forslag_StatusController(ApplicationDbContext first)
+    {
+        _first = first;
+    }
+    public IActionResult Index()
+    {
+        IEnumerable<Forslag_Status> objForslag_StatusList = _first.Forslag_Status;
+        return View(objForslag_StatusList);
+    }
+
+    //GET
+    public IActionResult Opprett()
+    {
+        return View();
+    }
+
+}
